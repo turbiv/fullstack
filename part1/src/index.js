7e1fbc1b17eb37f41d431d4b33924340f7f9e267
Ivan Turbin <ivan.turbin@gmail.com>
2019-07-12 17:21:28 +0300

    Added second part of part 1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,84 +1,49 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({handleClick, rating}) =>{
+const Button = ({handleClick, text}) =>{
   return(
-    <button onClick={handleClick}>
-      {rating}
-    </button>
+      <button onClick={handleClick}>
+        {text}
+      </button>
   );
 };
 
-const Statistic = ({text, value}) => {
-  return(
-    <tr>
-      <td>
-        <p>{text} {value}</p>
-      </td>
-    </tr>
-  );
-};
+const App = (props) => {
+  const [selected, setSelected] = useState('If it hurts, do it more often');
+  const [votes, setVotes] = useState({ 0:0,1:0,2:0,3:0,4:0,5:0,6:0});
 
-const Statistics = ({good, neutral, bad}) => {
-
-  const allfeedback = good + bad + neutral;
-
-  const calc_percent = () => {
-    if(allfeedback !== 0) {
-      return (good / allfeedback) * 100
-    }
-    return 0
+  const placevote = () =>{
+    votes[props.anecdotes.indexOf(selected)] += 1;
+    return votes
   };
 
-  const avarage_score = () => {
-    if(allfeedback !== 0) {
-      const score =good + -Math.abs(bad);
-      return score / allfeedback
-    }
-    return 0
+  const totalvotes = () =>{
+    return votes[props.anecdotes.indexOf(selected)]
   };
-
-  if(allfeedback === 0){
-    return(
-      <div>
-        <h1>Statistics</h1>
-        <p>No feedback given</p>
-      </div>
-    );
-  }
-
-  return(
-    <div>
-      <h1>Statistics</h1>
-      <table>
-        <Statistic text={"Good:"} value={good} />
-        <Statistic text={"Neutreal:"} value={neutral} />
-        <Statistic text={"Bad:"} value={bad} />
-        <Statistic text={"All:"} value={allfeedback} />
-        <Statistic text={"Avarage:"} value={avarage_score()} />
-        <Statistic text={"Positive:"} value={calc_percent() + "%"} />
-      </table>
-    </div>
-  )
-};
-
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <h1>Give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} rating={"Good"} />
-      <Button handleClick={() => setNeutral(neutral + 1)} rating={"Neutral"} />
-      <Button handleClick={() => setBad(bad + 1)} rating={"Bad"} />
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      {selected}
+      <p>Current votes on this anecdote: {votes[0]}</p>
+      <p>
+        <Button handleClick={() => setSelected(props.anecdotes[Math.floor(Math.random()*props.anecdotes.length)])} text={"Next anecdote"} />
+        <Button handleClick={() => setVotes(placevote())} text={"Vote"} />
+      </p>
     </div>
-  )
+  );
 };
 
-ReactDOM.render(<App />,
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 );
---
+++ part1/src/index_part1.11.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({handleClick, rating}) =>{
+  return(
+    <button onClick={handleClick}>
+      {rating}
+    </button>
+  );
+};
+
+const Statistic = ({text, value}) => {
+  return(
+    <tr>
+      <td>
+        <p>{text} {value}</p>
+      </td>
+    </tr>
+  );
+};
+
+const Statistics = ({good, neutral, bad}) => {
+
+  const allfeedback = good + bad + neutral;
+
+  const calc_percent = () => {
+    if(allfeedback !== 0) {
+      return (good / allfeedback) * 100
+    }
+    return 0
+  };
+
+  const avarage_score = () => {
+    if(allfeedback !== 0) {
+      const score =good + -Math.abs(bad);
+      return score / allfeedback
+    }
+    return 0
+  };
+
+  if(allfeedback === 0){
+    return(
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
+  return(
+    <div>
+      <h1>Statistics</h1>
+      <table>
+        <Statistic text={"Good:"} value={good} />
+        <Statistic text={"Neutreal:"} value={neutral} />
+        <Statistic text={"Bad:"} value={bad} />
+        <Statistic text={"All:"} value={allfeedback} />
+        <Statistic text={"Avarage:"} value={avarage_score()} />
+        <Statistic text={"Positive:"} value={calc_percent() + "%"} />
+      </table>
+    </div>
+  )
+};
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} rating={"Good"} />
+      <Button handleClick={() => setNeutral(neutral + 1)} rating={"Neutral"} />
+      <Button handleClick={() => setBad(bad + 1)} rating={"Bad"} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+};
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+);
