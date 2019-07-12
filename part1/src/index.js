84076a5b749bd556d20908c93d8ba11d29c5250a
Ivan Turbin <ivan.turbin@gmail.com>
2019-07-12 15:05:43 +0300

    part1 mandatory completed

--- part1/src/index.js
+++ part1/src/index.js
@@ -9,29 +9,64 @@ const Button = (props) =>{
   );
 };
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const Statistic = ({text, value}) => {
+  return(
+    <tr>
+      <td>
+        <p>{text} {value}</p>
+      </td>
+    </tr>
+  );
+};
+
+const Statistics = ({good, neutral, bad}) => {
+
   const allfeedback = good + bad + neutral;
 
   const calc_percent = () => {
     if(allfeedback !== 0) {
       return (good / allfeedback) * 100
-    }else{
-      return 0
     }
+    return 0
   };
 
   const avarage_score = () => {
     if(allfeedback !== 0) {
-      const score = good + -Math.abs(bad);
-      return score/allfeedback
-    }else{
-      return 0
+      const score =good + -Math.abs(bad);
+      return score / allfeedback
     }
+    return 0
   };
+
+  if(allfeedback === 0){
+    return(
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
+  return(
+    <div>
+      <h1>Statistics</h1>
+      <table>
+        <Statistic text={"Good:"} value={good} />
+        <Statistic text={"Neutreal:"} value={neutral} />
+        <Statistic text={"Bad:"} value={bad} />
+        <Statistic text={"All:"} value={allfeedback} />
+        <Statistic text={"Avarage:"} value={avarage_score()} />
+        <Statistic text={"Positive:"} value={calc_percent() + "%"} />
+      </table>
+    </div>
+  )
+};
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
@@ -39,13 +74,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} rating={"Good"} />
       <Button handleClick={() => setNeutral(neutral + 1)} rating={"Neutral"} />
       <Button handleClick={() => setBad(bad + 1)} rating={"Bad"} />
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {allfeedback}</p>
-      <p>Avarage: {avarage_score()}</p>
-      <p>Positive: {calc_percent()}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 };
