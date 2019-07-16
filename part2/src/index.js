63f9c54691809361597d893317eecd259f0bb420
Ivan Turbin <ivan.turbin@gmail.com>
2019-07-16 16:36:02 +0300

    Part 2 start

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,63 +1,34 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({handleClick, text}) =>{
-  return(
-      <button onClick={handleClick}>
-        {text}
-      </button>
-  );
-};
-
-
-
-const App = (props) => {
-  const [selected, setSelected] = useState('If it hurts, do it more often');
-  const [votes, setVotes] = useState([0,0,0,0,0,0,0]);
-  const randomanecdote = Math.floor(Math.random()*props.anecdotes.length);
-
-  const placevote = () =>{
-    const copy = [...votes];
-    copy[props.anecdotes.indexOf(selected)] +=1;
-    return copy
-  };
-
-  const Topanecdote = () =>{
-    const copy = [...votes];
-    let maxvote = copy.indexOf(Math.max(...copy));
-    console.log(maxvote);
-
-    return(
-        <div>
-          <h1>Anecdote with most votes</h1>
-          <p>{anecdotes[maxvote]}</p>
-          <p>Votes: {copy[maxvote]}</p>
-        </div>
-    );
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   };
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      {selected}
-      <p>Current votes on this anecdote: {votes[props.anecdotes.indexOf(selected)]}</p>
-      <p>
-        <Button handleClick={() => setSelected(props.anecdotes[randomanecdote])} text={"Next anecdote"} />
-        <Button handleClick={() => setVotes(placevote())} text={"Vote"} />
-      </p>
-      <Topanecdote/>
+      <Course course={course} />
     </div>
-  );
+  )
 };
-
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-];
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
