ddc724a51f6b3e0382623f9c8640d12152427b7d
Ivan Turbin <ivan.turbin@gmail.com>
2019-07-19 15:18:46 +0300

    2b last problem

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,44 +1,91 @@
 import React, { useState } from 'react'
 import ReactDOM from "react-dom";
 
+const Renderpersons = ({persons, filter}) =>{
+  return(persons.map(person => {
+    if(person.name.includes(filter)){
+      return(<p>{person.name + " " + person.number}</p>)
+    }
+  }));
+};
+
+const Searchfilter = ({handle, value}) =>{
+  return(
+  <form>
+    <div>
+      filter shown with<input onChange={handle} value={value}/>
+    </div>
+  </form>);
+};
+
+const Addpeople = ({onsubmit, onNameChange, onNumberChange, namevalue, numbervalue}) =>{
+  return(
+  <form onSubmit={onsubmit}>
+    <div>
+      name: <input onChange={onNameChange} value={namevalue}/>
+    </div>
+    <div>number: <input onChange={onNumberChange} value={numbervalue}/></div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>);
+};
+
 const App = () => {
-    const [ persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
-    ]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+
     const [ newName, setNewName ] = useState('');
+    const [newNumber,setNewNumber] = useState('');
+    const [filter,setFilter] = useState('');
 
     const addPerson = (event) =>{
-        event.preventDefault();
-        console.log("Button clikced", event.target);
-        console.log(persons.includes({name: newName}));
-        console.log(persons);
-        if(persons.includes({name: newName})){
-            alert(event.target.value + " is in the phonebook");
-            return
-        };
-
-        setPersons(persons.concat({name: newName}));
+      event.preventDefault();
+      if(newNumber.length === 0 || newName.length === 0){
+        alert("Both inputs have to be filled in");
+        setNewNumber('');
         setNewName('');
+      }
+      if(persons.some(e => e.name === newName)){
+          alert(newName + " is in the phonebook");
+          setNewName('');
+          return
+      }
+
+      if(persons.some(e => e.number === newNumber)){
+        alert(newNumber + " is in the phonebook");
+        setNewNumber('');
+        return
+      }
+        setPersons(persons.concat({name: newName, number: newNumber}));
+        setNewName('');
+        setNewNumber('');
     };
 
     const handlePersonChange = (event) =>{
-        console.log(event.target.value);
         setNewName(event.target.value)
+    };
+
+    const handleNumberChange = (event) =>{
+      setNewNumber(event.target.value)
+    };
+
+    const handleFilterChange = (event) =>{
+      setFilter(event.target.value)
     };
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <form onSubmit={addPerson}>
-                <div>
-                    name: <input onChange={handlePersonChange} value={newName}/>
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
+            <Searchfilter handle={handleFilterChange} value={filter}/>
+            <h2>Add a new</h2>
+            <Addpeople onsubmit={addPerson} onNameChange={handlePersonChange} onNumberChange={handleNumberChange} namevalue={newName} numbervalue={newNumber}/>
             <h2>Numbers</h2>
-            ...
+            <Renderpersons persons={persons} filter={filter}/>
         </div>
     )
 };
