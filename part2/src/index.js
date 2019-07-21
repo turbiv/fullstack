e3d67b445083535c435a5edbf10f9c0d61d36514
IvanTurbin <ivan.turbin@gmail.com>
2019-07-21 23:49:01 +0300

    Countries 2b

--- part2/src/index.js
+++ part2/src/index.js
@@ -3,94 +3,47 @@ import ReactDOM from "react-dom";
 import axios from 'axios'
 
 
-const Renderpersons = ({persons, filter}) =>{
-  return(persons.map(person => {
-    if(person.name.includes(filter)){
-      return(<p>{person.name + " " + person.number}</p>)
-    }
-  }));
+const Rendercountries = ({countires, filter}) =>{
+    return(countires.map(country => {
+        if(country.name.includes(filter)){
+            return(<p>{country.name}</p>)
+        }
+    }));
 };
 
 const Searchfilter = ({handle, value}) =>{
-  return(
-  <form>
-    <div>
-      filter shown with<input onChange={handle} value={value}/>
-    </div>
-  </form>);
-};
-
-const Addpeople = ({onsubmit, onNameChange, onNumberChange, namevalue, numbervalue}) =>{
-  return(
-  <form onSubmit={onsubmit}>
-    <div>
-      name: <input onChange={onNameChange} value={namevalue}/>
-    </div>
-    <div>number: <input onChange={onNumberChange} value={numbervalue}/></div>
-    <div>
-      <button type="submit">add</button>
-    </div>
-  </form>);
+    return(
+        <form>
+            <div>
+                filter shown with<input onChange={handle} value={value}/>
+            </div>
+        </form>);
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-    const [ newName, setNewName ] = useState('');
-    const [newNumber,setNewNumber] = useState('');
+    const [country, setCountry] = useState([]);
     const [filter,setFilter] = useState('');
 
     useEffect(() =>{
-      console.log("effect is in use")
-      axios
-          .get('http://localhost:3001/persons')
-          .then(response =>{
-            setPersons(response.data)
-          })
+        console.log("effect is in use");
+        axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then(response =>{
+                console.log(response.data)
+                setCountry(response.data)
+            })
     },[]);
 
-    const addPerson = (event) =>{
-      event.preventDefault();
-      if(newNumber.length === 0 || newName.length === 0){
-        alert("Both inputs have to be filled in");
-        setNewNumber('');
-        setNewName('');
-      }
-      if(persons.some(e => e.name === newName)){
-          alert(newName + " is in the phonebook");
-          setNewName('');
-          return
-      }
-
-      if(persons.some(e => e.number === newNumber)){
-        alert(newNumber + " is in the phonebook");
-        setNewNumber('');
-        return
-      }
-        setPersons(persons.concat({name: newName, number: newNumber}));
-        setNewName('');
-        setNewNumber('');
-    };
-
-    const handlePersonChange = (event) =>{
-        setNewName(event.target.value)
-    };
-
-    const handleNumberChange = (event) =>{
-      setNewNumber(event.target.value)
-    };
 
     const handleFilterChange = (event) =>{
-      setFilter(event.target.value)
+        setFilter(event.target.value)
     };
 
     return (
         <div>
-            <h2>Phonebook</h2>
+            <h2>Country finder</h2>
             <Searchfilter handle={handleFilterChange} value={filter}/>
-            <h2>Add a new</h2>
-            <Addpeople onsubmit={addPerson} onNameChange={handlePersonChange} onNumberChange={handleNumberChange} namevalue={newName} numbervalue={newNumber}/>
-            <h2>Numbers</h2>
-            <Renderpersons persons={persons} filter={filter}/>
+            <Rendercountries countires={country} filter={filter}/>
         </div>
     )
 };
