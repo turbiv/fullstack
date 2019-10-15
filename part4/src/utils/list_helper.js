c373eae686147da627716ec4b3c54d4ec6a8f82f
Ivan Turbin <ivan.turbin@gmail.com>
2019-10-15 17:08:11 +0300

    unit tests for 4.3->4.7

--- part4/src/utils/list_helper.js
+++ part4/src/utils/list_helper.js
@@ -1,3 +1,9 @@
+var _ = require('lodash');
+var _ = require('lodash/core');
+var fp = require('lodash/fp');
+var loadsharray = require('lodash/array');
+var loadshcollection = require('lodash/collection');
+
 const dummy = (blogs) => {
   return 1
 }
@@ -6,7 +12,52 @@ const totalLikes = (blogs) =>{
   return blogs[0].likes
 }
 
+const favoriteBlog = (blogs) => {
+  var maxlikes = null;
+  var result = null;
+  blogs.forEach((item, iter) => {
+    if(maxlikes < blogs[iter].likes){
+      maxlikes = blogs[iter].likes
+      result = blogs[iter]
+  }
+  })
+  return result
+}
+
+const mostBlogs = (blogs) => {
+  const sortedblogs = [];
+  var maxblogs = null;
+  var result = null;
+
+  const names = blogs.map((item) => item.author)
+  const groupedBlogs = loadshcollection.groupBy(blogs, "author")
+  names.forEach((item, iter) => {
+    if(maxblogs < groupedBlogs[item].length){
+      maxblogs = groupedBlogs[item].length
+      result = groupedBlogs[item]
+    }
+  })
+
+  return {author: result[0].author, blogs: result.length}
+}
+
+const mostLikes = blogs =>{
+  var maxlikes = null;
+  var result = null;
+  blogs.forEach((item, iter) => {
+    if(maxlikes < blogs[iter].likes){
+      maxlikes = blogs[iter].likes
+      result = blogs[iter]
+    }
+  })
+
+  return {author: result.author, likes: result.likes}
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
