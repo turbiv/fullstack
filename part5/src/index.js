0595e86c7369c2ac9d2462d80d63ae3fb5eb7bae
IvanTurbin <ivan.turbin@gmail.com>
2019-11-06 22:08:59 +0200

    login frontend

--- part5/src/index.js
+++ part5/src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from 'react'
+
+import ReactDOM from 'react-dom'
+import blogApi from "./services/blogs";
+import loginApi from "./services/login";
+
+const App = () =>{
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async (event) =>{
+    event.preventDefault();
+    console.log(password, username);
+    const user = await loginApi.login(username, password);
+    console.log(user);
+    setUser(user)
+  };
+
+  if(user === null){
+    return(
+      <div>
+        Please login
+        <form onSubmit={handleLogin}>
+          <div>
+            Username: <input value={username} type={"text"} onChange={({target}) => setUsername(target.value)}/>
+          </div>
+          <div>
+            Password: <input value={password} type={"password"} onChange={({target}) => setPassword(target.value)}/>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
+  }else{
+    return(<div><p>Logged in</p></div>)
+  }
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
