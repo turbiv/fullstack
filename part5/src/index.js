bbcef82229a408a2dde8b38d184b952813fafddc
IvanTurbin <ivan.turbin@gmail.com>
2019-11-07 19:10:30 +0200

    permanent login and logout

--- part5/src/index.js
+++ part5/src/index.js
@@ -8,13 +8,38 @@ const App = () =>{
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const displayblogs = [];
+
+  useEffect(async ()=>{
+    const blogs = await blogApi.getAll();
+
+    blogs.map(item => displayblogs.push(item))
+
+  },[]);
+
+  useEffect(() => {
+    const browserStorage = window.localStorage.getItem("loggedUser");
+    if(browserStorage){
+      setUser(browserStorage);
+      blogApi.setToken(browserStorage.token)
+    }
+  },[]);
 
   const handleLogin = async (event) =>{
     event.preventDefault();
     console.log(password, username);
     const user = await loginApi.login(username, password);
     console.log(user);
-    setUser(user)
+    window.localStorage.setItem(
+      'loggedUser', JSON.stringify(user)
+    );
+    setUser(user);
+    blogApi.setToken(user.token)
+  };
+
+  const handleLogout = () =>{
+    setUser(null);
+    blogApi.setToken("")
   };
 
   if(user === null){
@@ -33,7 +58,7 @@ const App = () =>{
       </div>
     )
   }else{
-    return(<div><p>Logged in</p></div>)
+    return(<div><button onClick={handleLogout}>Logout</button></div>)
   }
 };
 
--- part5/src/services/login.js
+++ part5/src/services/login.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3003/api/login/';
 
 const login = (username, password) => {
-  console.log(username)
+  console.log(username);
   const request = axios.post(baseUrl, {username: username, password: password});
   return request.then(response => response.data)
 };
