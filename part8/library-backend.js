d24bc592e400d5d47a84f4327894bbbbe86f5677
Ivan Turbin <ivan.turbin@f-secure.com>
2019-12-27 17:31:24 +0200

    x

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -103,7 +103,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -114,10 +114,17 @@ const resolvers = {
     authorCount: () => authors.length,
     allAuthors: () => authors,
     allBooks: (root, args) => {
-      if(!args.author){
+      if(!args.author && !args.genre){
         return books
       }
-      return books.filter(book => book.author === args.author)
+
+      if(args.author){
+        return books.filter(book => book.author === args.author)
+      }
+
+      if(args.genre){
+        return books.filter(book => book.genres.includes(args.genre))
+      }
     }
   },
   Author: {
